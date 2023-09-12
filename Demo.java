0e03bdc6325112d90f242b90ec76128fe07bd614
Kamakshi Goyal <kamakshi.goyal@BLRETV-7MG6J60.local>
2023-09-12 15:04:47 +0530

    My First Commit

---
+++ Demo.java
@@ -0,0 +1,13 @@
+
+// Java program to demonstrate BufferedReader
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Demo{
+	public static void main(String[] args)
+	{
+
+		// Printing the read line
+		System.out.println("Learning Java);
+	}
+}
